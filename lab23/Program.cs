46f76a2327118ea47f9e976b683d4ead422beef9
Doroshchenkov <doroshchenkov.d_ipz23@rcit.ukr.education>
2026-02-08 21:03:47 +0200

    lab23: ISP, DIP and constructor DI

---
+++ lab23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Lab23.Bad;
+using Lab23.Good;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("===== BAD (violates ISP + DIP) =====");
+        var bad = new BadPayrollSystem();
+        bad.ProcessPayroll(employeeId: 7, hours: 160, hourlyRate: 120);
+        Console.WriteLine();
+        Console.WriteLine("===== GOOD (ISP + DIP + Constructor DI) =====");
+        ISalaryCalculator calculator = new DefaultSalaryCalculator();
+        IReportExporter exporter = new TextReportExporter();
+        IPayrollRepository repository = new ConsolePayrollRepository();
+        var good = new PayrollSystem(calculator, exporter, repository);
+        good.ProcessPayroll(employeeId: 7, hours: 160, hourlyRate: 120);
+        Console.WriteLine("\nDone.");
+    }
+}
